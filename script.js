5fb12678d63ceb3557cd2b1c9bf67517fe8a56b9
Kenjie Tarasona-wsl <148972543+voidKing00@users.noreply.github.com>
2025-01-26 23:45:52 +0800

    Add event listeners to digit buttons
    concatenating their text content to a inputValue
    variable to be processed later for evaluation

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("test connection");
+const getDigitButtons = document.querySelectorAll(".left-buttons-container button");
+const digitButtons = Array.from(getDigitButtons);
+
+let inputValue = "";
+
+digitButtons.map(button => {
+    
+    button.addEventListener("click", () => {
+
+        alert(button.textContent);        
+        inputValue += button.textContent;
+        
+        console.log(inputValue);
+    });
+});
+
+
+
