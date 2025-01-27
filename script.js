63a5c151b3a88815cc489ac12ef5044bc7ce4c06
Kenjie Tarasona-wsl <148972543+voidKing00@users.noreply.github.com>
2025-01-28 04:39:37 +0800

    Make equal eval interoperable with op eval

--- script.js
+++ script.js
@@ -5,33 +5,16 @@ function reset(){
     inputValue = "";
 
 }
-
-function add(a, b){
-    
-    return a + b;
-
-}
-
-function subtract(a, b){
-    
-    return a - b;
-
-}
-
-function multiply(a, b){
-    
-    return a * b;
-}
-
-function divide(a, b){
-    
-    return a / b;
-}
-
 function evaluate(){
     
-    return calculationStack[1](+calculationStack[0], +calculationStack[2]);
+    let result = calculationStack[1](+calculationStack[0], +calculationStack[2]);
+    calculationStack = [];
+    return result;
 }
+function add(a, b) {return a + b};
+function subtract(a, b){return a - b;}
+function multiply(a, b){return a * b;}
+function divide(a, b){return a / b;}
 
 let buttonClickedOnce= false;
 let calculationStack = [];
@@ -43,18 +26,20 @@ const clearButton = digitButtons.pop();
 
 const screen = document.querySelector(".screen");
 
-digitButtons.map(button => {
-    
-    button.addEventListener("click", () => {
-        
-        if(!buttonClickedOnce){
-            
+digitButtons.map(button => {  
+
+    button.addEventListener("click", () => { 
+
+        if(!buttonClickedOnce){ 
+
             screen.textContent = "";
             buttonClickedOnce = true;
+
         }
 
         inputValue += button.textContent;
         screen.textContent += button.textContent;
+    
     });
 });
 
@@ -62,22 +47,19 @@ clearButton.addEventListener("click", reset);
 
 const operationFunctionArray = [add, subtract, multiply, divide];
 const operationFunctionSymbol = [' + ', ' - ', ' x ', ' / '];
-
 const fetchOperationButtons = document.querySelectorAll(".right-buttons-container button");
-
 const operationButtons = Array.from(fetchOperationButtons);
 const equalButton = operationButtons.pop();
-
 
 for(let i = 0; i < operationFunctionArray.length; i++){
     
     operationButtons[i].addEventListener("click", () => {
         
         if(inputValue === "") return;
-        let operation = operationFunctionArray[i];
 
-        // START
-        if(calculationStack.length === 0){
+        const operation = operationFunctionArray[i];
+
+        if(calculationStack.length === 0){ // Start state
             
             calculationStack.push(inputValue);
             calculationStack.push(operation);
@@ -91,57 +73,30 @@ for(let i = 0; i < operationFunctionArray.length; i++){
         if(calculationStack.length === 3){
 
             let result = evaluate();
-            calculationStack = [];
             calculationStack.push(result)
             calculationStack.push(operation);
-            
+
         }
 
         console.log(calculationStack);
         let operationSymbol = operationFunctionSymbol[i];
         screen.textContent += operationSymbol;
-        
         inputValue = "";
 
     });
 }
 
-// equalButton.addEventListener("click", () => {
+equalButton.addEventListener("click", () => {
 
-//     if(inputStack.length === 0){
-        
-//         console.log("Syntax Error: No Inputs")
-//         return;
-//     }
+    if(calculationStack.length === 0){return;}
+    if(inputValue === "") {return;}
 
-//     if(inputValue === "") {
+    if(calculationStack.length === 2){
 
-//         console.log("Syntax Error: Last input is an operator, failed to evaluate");
-//         return;
-
-//     }
-
-//     inputStack.push(inputValue);
-//     inputValue = "";
-
-//     inputStack = inputStack.map(input => {
-
-//         return +input;
-
-//     });
-
-//     screen.textContent = "";
+        calculationStack.push(inputValue);
+        let result = evaluate();
+        inputValue = `${result}`;
+        console.log(calculationStack);
+    }
     
-//     let result = evaluate(inputStack, operationStack);
-    
-//     if(result === Infinity){
-        
-//         alert("Division Error");
-//         reset();
-//         return;
-//     }
-
-//     screen.textContent = `= ${result}`
-    
-// });
-
+});
