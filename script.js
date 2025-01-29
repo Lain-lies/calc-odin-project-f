18e772b2feab218b71fcc4d5ead21d102df6e0ee
Kenjie Tarasona-wsl <148972543+voidKing00@users.noreply.github.com>
2025-01-30 03:40:26 +0800

    Add backspace btn and div error handling

--- script.js
+++ script.js
@@ -2,8 +2,39 @@ function reset(){
     
     screen.textContent = "0";
     buttonClickedOnce = false;
+    calculationStack = [];
     inputValue = "";
 
+}
+
+function divisionError(){
+    
+    alert("Division Error");
+    reset();
+
+}
+
+function deleteInput(){
+    
+    if(inputValue !== ""){
+
+        inputValue = inputValue.slice(0, inputValue.length - 1);
+        screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
+
+    }else if(inputValue === "" && calculationStack.length === 2){
+         
+        inputValue = calculationStack[0];
+        calculationStack = [];
+        screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
+
+    }
+    
+    if(calculationStack.length === 0 && inputValue === ""){
+
+        reset();
+        
+    }
+    
 }
 
 function evaluate(){
@@ -11,14 +42,14 @@ function evaluate(){
     let result = calculationStack[1](+calculationStack[0], +calculationStack[2]);
     calculationStack = [];
     return result;
+
 }
+
 
 function add(a, b) {return a + b};
 function subtract(a, b){return a - b;}
 function multiply(a, b){return a * b;}
-function divide(a, b){return a / b;}
-
-
+function divide(a, b){return b === 0 ?  null:  a / b}
 
 let buttonClickedOnce = false;
 let calculationStack = [];
@@ -27,10 +58,11 @@ let inputValue = "";
 const fetchDigitButtons = document.querySelectorAll(".left-buttons-container button");
 const digitButtons = Array.from(fetchDigitButtons);
 
-const fetchDigitLastTwoButtons = digitButtons.splice(-2, 2);
+const fetchDigitLastThreeButtons = digitButtons.splice(-3, 3);
 
-const pointButton = fetchDigitLastTwoButtons[0];
-const clearButton = fetchDigitLastTwoButtons[1];
+const pointButton = fetchDigitLastThreeButtons[0];
+const deleteButton = fetchDigitLastThreeButtons[1];
+const clearButton = fetchDigitLastThreeButtons[2];
 
 const screen = document.querySelector(".screen");
 
@@ -63,16 +95,18 @@ pointButton.addEventListener("click", () => {
         
         return;
     }
-    
+
     inputValue += pointButton.textContent;
     screen.textContent += pointButton.textContent;
     console.log(inputValue);
 });
 
+deleteButton.addEventListener("click", deleteInput);
+
 clearButton.addEventListener("click", reset);
 
 const operationFunctionArray = [add, subtract, multiply, divide];
-const operationFunctionSymbol = [' + ', ' - ', ' x ', ' / '];
+const operationFunctionSymbol = ['+', '-', 'x', '/'];
 const fetchOperationButtons = document.querySelectorAll(".right-buttons-container button");
 const operationButtons = Array.from(fetchOperationButtons);
 const equalButton = operationButtons.pop();
@@ -107,7 +141,7 @@ for(let i = 0; i < operationFunctionArray.length; i++){
 
         }
 
-        console.log(calculationStack);
+        console.log(`from operationbtn : ${calculationStack}`);
         screen.textContent += operationSymbol;
         inputValue = "";
 
@@ -123,9 +157,15 @@ equalButton.addEventListener("click", () => {
 
         calculationStack.push(inputValue);
         let result = evaluate();
+
+        if(result === null){
+            
+            alert("Division Error");
+            reset();
+            return;
+        }
         screen.textContent = result;
         inputValue = `${result}`;
-        console.log(calculationStack);
     }
     
 });
